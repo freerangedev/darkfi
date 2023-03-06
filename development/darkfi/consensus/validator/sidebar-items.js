b74f31eff8669222c8c12b887d26426c305619aa
parazyd <41898282+github-actions[bot]@users.noreply.github.com>
2023-03-06 17:42:22 +0000

    Generate mdbook

--- development/darkfi/consensus/validator/sidebar-items.js
+++ development/darkfi/consensus/validator/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["ValidatorState","This struct represents the state of a validator node."]],"type":[["ValidatorStatePtr","Atomic pointer to validator state."],["VerifyingKeyMap",""]]};
+window.SIDEBAR_ITEMS = {"struct":[["ValidatorState","This struct represents the state of a validator node."]],"type":[["ValidatorStatePtr","Atomic pointer to validator state."]]};
