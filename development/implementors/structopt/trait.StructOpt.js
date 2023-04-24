3f0705079b80b114ed809c5c4a59ebaa18dfbb65
parazyd <41898282+github-actions[bot]@users.noreply.github.com>
2023-04-24 22:32:10 +0000

    Generate mdbook

--- development/implementors/structopt/trait.StructOpt.js
+++ development/implementors/structopt/trait.StructOpt.js
@@ -1,11 +1,11 @@
 (function() {var implementors = {
 "darkfi":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"darkfi/net/settings/struct.SettingsOpt.html\" title=\"struct darkfi::net::settings::SettingsOpt\">SettingsOpt</a>"]],
 "darkfid":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"darkfid/struct.Args.html\" title=\"struct darkfid::Args\">Args</a>"]],
+"darkirc":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"darkirc/settings/struct.Args.html\" title=\"struct darkirc::settings::Args\">Args</a>"]],
 "faucetd":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"faucetd/struct.Args.html\" title=\"struct faucetd::Args\">Args</a>"]],
 "fud":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"fud/struct.Args.html\" title=\"struct fud::Args\">Args</a>"]],
 "genevd":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"genevd/struct.Args.html\" title=\"struct genevd::Args\">Args</a>"]],
 "ircd":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"ircd/settings/struct.Args.html\" title=\"struct ircd::settings::Args\">Args</a>"]],
-"ircd2":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"ircd2/settings/struct.Args.html\" title=\"struct ircd2::settings::Args\">Args</a>"]],
 "lilith":[["impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"lilith/config/struct.Args.html\" title=\"struct lilith::config::Args\">Args</a>"]],
 "structopt":[],
 "structopt_toml":[],
