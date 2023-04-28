42f0eebc12c6cac68893a9ad2bf7caae7b006bfa
parazyd <41898282+github-actions[bot]@users.noreply.github.com>
2023-04-28 21:07:51 +0000

    Generate mdbook

--- development/darkfi_sdk/util/sidebar-items.js
+++ development/darkfi_sdk/util/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["GET_SYSTEM_TIME_FAILED"],"fn":["get_system_time","get_system_time_"]};
+window.SIDEBAR_ITEMS = {"constant":["CALL_FAILED"],"fn":["get_object_bytes","get_object_bytes_","get_object_size","get_object_size_","get_system_time","get_system_time_","put_object_bytes","put_object_bytes_","set_return_data","set_return_data_"]};
